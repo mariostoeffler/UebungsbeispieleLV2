6db5e98756937d396270c60c05884b2aa938b89d
mariostoeffler <52000392@edu.campus02.at>
2022-09-26 20:02:38 +0200

    final version Beispiel 2; Class WinterreifenpflichtDemo added;

---
+++ src/WinterreifenpflichtDemo.java
@@ -0,0 +1,27 @@
+
+
+public class WinterreifenpflichtDemo {
+
+    public static void main(String[] args) {
+
+        boolean Sommerreifen = isWinterreifenPflicht(14,false);
+        System.out.println("Winterreifen sind nicht erforderlich");
+
+        boolean Winterreifen = isWinterreifenPflicht(3, false);
+        System.out.println("Bitte Winterreifen verwenden");
+
+        boolean Winterreifen2 = isWinterreifenPflicht(6, true);
+        System.out.println("Bitte Winterreifen verwenden");
+
+    }
+
+    public static boolean isWinterreifenPflicht (int temperatur, boolean rutschigeFahrbahn) {
+        boolean Winterreifen;
+
+        if (temperatur < 10 && rutschigeFahrbahn == true || temperatur < 4) {
+            return Winterreifen = true;
+        }
+        else
+            return Winterreifen = false;
+    }
+}
