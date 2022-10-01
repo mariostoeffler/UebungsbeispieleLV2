0b25ac8c22fe6858073f81a0f6c42c58a04f1178
mariostoeffler <52000392@edu.campus02.at>
2022-10-01 21:33:14 +0200

    Scanner for int added in TurmDemo;

--- src/TurmDemo.java
+++ src/TurmDemo.java
@@ -1,22 +1,26 @@
+import java.util.Scanner;
+
 public class TurmDemo {
 
+
     public static void main(String[] args) {
-        
-        int ergebnis = getTurmRechnung(114);
+        Scanner scanner1 = new Scanner(System.in);
+        System.out.println("Gib einen Integer-Wert ein: ");
+        int input2 = scanner1.nextInt();
+        int ergebnis = getTurmRechnung(input2);
+
         System.out.println("Endergebnis = " + ergebnis);
-        
     }
 
-    
-    
-    public static int getTurmRechnung (int input) {
-        for (int i = 2; i < 10 ; i++) {
+
+    public static int getTurmRechnung(int input) {
+        for (int i = 2; i < 10; i++) {
             System.out.print(input + " * " + i + " = ");
             input = input * i;
             System.out.println(input);
 
         }
-        for (int i = 2; i < 10 ; i++) {
+        for (int i = 2; i < 10; i++) {
             System.out.print(input + " / " + i + " = ");
             input = input / i;
             System.out.println(input);
